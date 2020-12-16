ddb1adb8cf5cb804a0e83d26f13eea706675b9d8
StaRadian <47676364+StaRadian@users.noreply.github.com>
2020-12-16 11:42:28 +0900

    hs_update

--- src/MN_Run.kt
+++ src/MN_Run.kt
@@ -1,7 +1,7 @@
 class MN_Run {
     fun Run() {
         //println("Hello Run!")
-        val MN_win = MN_WIN()
+        val MN_win = MN_Triangle()
         MN_win.Init()
         MN_win.Loop()
         MN_win.Destroy()
