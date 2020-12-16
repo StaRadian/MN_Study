ba8b1709c19313308a892986efe7683e2d9a7129
bjio99 <76077656+bjio99@users.noreply.github.com>
2020-12-15 23:25:29 -0800

    Create ToToKia's space.kt
    호잇

---
+++ src/ToToKia's space.kt
@@ -0,0 +1,27 @@
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.opengl.GL
+import org.lwjgl.opengles.GLES20.*
+
+fun render(win: Long){
+    glClearColor(0.0f,1.0f,0.0f,1.0f)
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glfwSwapBuffers(win)
+}
+
+fun main(){
+    glfwInit()
+    val window = glfwCreateWindow(640,480, "ToToKia", 0, 0)
+
+    glfwMakeContextCurrent(window)
+    GL.createCapabilities()
+
+
+    while(!glfwWindowShouldClose(window)){
+        render(window)
+        glfwPollEvents()
+    }
+
+    glfwDestroyWindow(window)
+    glfwTerminate()
+}
